35ff01aa0d326f240f0cd384716b1efc5cc9a6bc
nicolaureyes <nicolau.reyes@mataro.epiaedu.cat>
2024-02-22 19:43:31 +0100

    Empezando codigo memory

---
+++ app/src/main/java/com/example/pj_infantil/level1memory.kt
@@ -0,0 +1,12 @@
+package com.example.pj_infantil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class level1memory  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.level1memory)
+    }
+}
