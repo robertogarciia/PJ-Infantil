4fa68ae7aec2dd1b5383372f921a07ee31f6a447
Roberto García <robertomimero@gmail.com>
2024-02-22 19:41:49 +0100

    addingMenuPri

--- app/src/main/java/com/example/pj_infantil/MainActivity.kt
+++ app/src/main/java/com/example/pj_infantil/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.pj_infantil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_menu_principal)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            else -> super.onOptionsItemSelected(item)
+        }
+
+        return(super.onOptionsItemSelected(item));
+    }
+
 
 
 }
