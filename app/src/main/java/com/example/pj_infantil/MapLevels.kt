987f479e54798f1e17a82e8bc4aa10012a62449e
Roberto García <robertomimero@gmail.com>
2024-02-23 19:49:24 +0100

    funciona ya

--- app/src/main/java/com/example/pj_infantil/MapLevels.kt
+++ app/src/main/java/com/example/pj_infantil/MapLevels.kt
@@ -17,6 +17,7 @@ class MapLevels: AppCompatActivity() {
             setContentView(R.layout.firsts_levels)
 
         }
+
         window.decorView.systemUiVisibility = (
                 View.SYSTEM_UI_FLAG_IMMERSIVE
                         or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
